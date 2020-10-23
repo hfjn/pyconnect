e6c811a3ab11585023ed366ac96689d977905ba5
Jannik Hoffjann <hi@hfjn.de>
2020-10-23 20:47:43 +0200

    More cleanup

--- test_pypi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python3 -m virtualenv venv_test
-. venv_test/bin/activate
-pip install pyconnect --no-cache-dir
-python -m pyconnect.pyconnectsink
-echo "Nice, no errors :)"
-deactivate
-rm -rf venv_test
